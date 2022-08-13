845baa322ada4f234939fb0825b92476d2d302e3
Constanza Osorio <ceosorio@uc.cl>
2022-08-13 13:39:34 -0400

    feat: buld main function with parameters

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from methods import *
+import pandas as pd
 
-def main():
-  pass
+def main(json_file_path, method):
+  raw_tweets = pd.read_json(json_file_path, lines=True)
+  raw_tweets = raw_tweets[raw_tweets['lang']=='en']
+
+  print(method(raw_tweets))
+
 
 if __name__ == "__main__":
-  main()
+  # Here you can change the method used
+  main('farmers-protest-tweets-2021-03-5.json', top_10_users)
