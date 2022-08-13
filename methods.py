65073414af780009835178c510e6d966e1bd943d
Constanza Osorio <ceosorio@uc.cl>
2022-08-13 13:39:58 -0400

    feat: top10 retweets and users

--- methods.py
+++ methods.py
@@ -1,8 +1,22 @@
+import pandas as pd
+
 def top_10_retweeted(data):
-  pass
+  retweet_count = data.nlargest(10, 'retweetCount')
+  return retweet_count['content', 'retweetCount']
 
 def top_10_users(data):
-  pass
+  users = pd.json_normalize(data['user'])
+  users.drop(['description', 'linkTcourl', 'url'], axis=1, inplace=True)
+  users = pd.DataFrame(users)
+  users.drop_duplicates(subset=['username'], inplace=True)
+  usernames = []
+
+  for user in data['user']:
+      username = user['username']
+      usernames.append(username)
+  data['username'] = usernames
+  
+  return data['username'].value_counts().head(10)
 
 def top_10_days(data):
   pass
